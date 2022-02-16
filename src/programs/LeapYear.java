4c408abb88cc370b67f77393fb8922a980c9288f
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-16 15:18:18 +0530

    Print the year is a Leap Year or not

---
+++ src/programs/LeapYear.java
@@ -0,0 +1,27 @@
+package programs;
+import java.util.*;
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			    Scanner y=new Scanner(System.in);
+			    System.out.println("Enter the Year:");
+			    int year=y.nextInt();
+			    	if(year>999 && year<10000)
+			    	{
+			        	if(year%4==0 && year%100==0 && year%400==0)
+			                {
+			                    System.out.println(year + " is Leap Year");
+			                }
+			        	else
+			        	{
+			        			System.out.println(year + " is Not a Leap Year");
+			        	}
+			    	}
+			    	else
+			    	{
+			    		System.out.println(year + " is Invalid Year");
+			    	}
+			    y.close();
+	}
+}
